fbe839686ef055efdc4eae1b20cf8bb127c2dbcb
xzee21 <91794950+xzee21@users.noreply.github.com>
2021-11-24 15:28:19 +0700

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'dzee22/LXZ'
+let repository = 'xzee21/oxzee'
 let branch = 'main'
 
 async function handler(m, { text }) {
