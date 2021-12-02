8e3cba1aa3a4d10c03a3bdfdfc2501a4d3d14763
Yumiaaa17 <95274326+Yumiaaa17@users.noreply.github.com>
2021-12-02 20:39:19 +0700

    Update and rename plugins/done.js to plugin/done.js

--- plugin/done.js
+++ plugin/done.js
@@ -1,11 +1,12 @@
 let handler = async m => m.reply(`
 *STATUS PESANAN*
 
-*BERHASIL ✅*
+*𝑆𝑡𝑎𝑡𝑢𝑠 𝑃𝑒𝑠𝑎𝑛𝑎𝑛 : 𝑆𝑒𝑙𝑒𝑠𝑎𝑖*
 
-*Jangan Lupa Order Kembali Ya🤗*
+*𝑇𝑒𝑟𝑖𝑚𝑎 𝐾𝑎𝑠𝑖ℎ 𝑑𝑢𝑖𝑡𝑛𝑦𝑎 𝑗𝑎𝑛𝑔𝑎𝑛 𝑙𝑢𝑝𝑎 𝑜𝑟𝑑𝑒𝑟 𝑙𝑎𝑔𝑖*
 
-*© Lyra*
+- 𝑌𝑢𝑚𝑖><
+
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['done']
 handler.tags = ['store']
